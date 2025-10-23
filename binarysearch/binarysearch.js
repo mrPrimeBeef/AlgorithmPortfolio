3fe649f24afdfbc3c0a81f349ce13436f0e58233
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-10-23 10:53:02 +0200

    binary search algo start

---
+++ binarysearch/binarysearch.js
@@ -0,0 +1,12 @@
+function binarySearch(searchFor, values){
+return 
+}
+
+// - `found` - en boolean der indikerer om searchFor blev fundet eller ej
+// - `index` - et number der angiver hvilket index værdien blev fundet på, eller `-1` hvis den ikke blev fundet.
+// - `iterations` - et number der angiver antallet af iterationer funktionen gennemløb før den enten fandt, eller opgav at finde værdien.
+
+// den skal gå ud fra at arrayet er sorteret - den må gerne fejle, altså returnere !found og index -1 hvis den skal søge i et usorteret array. Men ikke gå ind i et uendeligt loop eller crashe på anden vis.
+
+
+// npx mocha -> to run test.js
