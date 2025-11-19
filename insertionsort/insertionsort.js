e5c564a4d72c0016fb56a284243aa5234efcb76d
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-11-19 13:07:47 +0100

    insertion ssort update with swap and fixed with interations and using isSorted algo

--- insertionsort/insertionsort.js
+++ insertionsort/insertionsort.js
@@ -1,10 +1,12 @@
-// implemented with shifting 
+import { isSorted } from "../issorted/issorted.js";
+
+// implemented with shifting
 
 const arr = [1, 8, 9, 4, 6, 7, 2, 5, 3];
 
-export function insertionSort(arr) {
+export function insertionSortShift(arr) {
   let iterations = 0;
-  
+
   for (let i = 1; i < arr.length; i++) {
     let key = arr[i];
     let j = i - 1;
@@ -16,11 +18,29 @@ export function insertionSort(arr) {
     }
     arr[j + 1] = key;
   }
-  
-  return { arr: arr, iterations: iterations, sorted: true };
+
+  return { arr: arr, iterations: iterations, sorted: isSorted(arr) };
 }
 
-console.log(insertionSort(arr));
+console.log(insertionSortShift(arr));
 
+// implemented with swap
 
-// implemented with swapping -> 
+const arr1 = [1, 8, 9, 4, 6, 7, 2, 5, 3];
+
+export function insertionSortSwap(arr) {
+  let iterations = 0;
+
+  for (let i = 1; i < arr.length + 1; i++) {
+    let j = i - 1;
+    while (j >= 0 && arr[j - 1] > arr[j]) {
+      [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+      j--;
+      iterations++;
+    }
+  }
+
+  return { arr: arr, iterations: iterations, sorted: isSorted(arr) };
+}
+
+console.log(insertionSortSwap(arr1));
